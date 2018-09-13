7bf75723334402492d72a1b3b6a72a5be7b44e3c
Piyush Chaudhari <Piyush.Chaudhari@futureretail.in>
2018-09-13 12:50:54 +0530

    save country

--- src/main/java/com/ehcache/example/controller/CountryController.java
+++ src/main/java/com/ehcache/example/controller/CountryController.java
@@ -8,10 +8,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ehcache.example.entity.Country;
 import com.ehcache.example.service.CountryService;
 
 @RestController
@@ -27,6 +29,14 @@ public class CountryController {
 	public ResponseEntity<Object> list(){
 		Map<String, Object> resultMap = new HashMap<>();
 		resultMap.put("countryList", countryService.list());
+		return new ResponseEntity<>(resultMap, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/create",method=RequestMethod.POST)
+	public ResponseEntity<Object> create(@RequestBody Country country){
+		Map<String, Object> resultMap = new HashMap<>();
+		logger.info("create:> {} ",country);
+		resultMap.put("country", countryService.save(country));
 		return new ResponseEntity<>(resultMap, HttpStatus.OK);
 	}
 }
